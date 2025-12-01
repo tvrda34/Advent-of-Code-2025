84170c6e466be453a083dbd447d1609c1cde20f9
tvrda34 <tvrtko.ivasic3@gmail.com>
2025-12-01 17:52:06 +0100

    AoC day 1 solutions

---
+++ README.md
@@ -0,0 +1,18 @@
+# Advent of Code 2025 solutions repo
+
+I am learning Rust and trying to solve AoC puzzles without LLMs.
+
+> Code is probably shit because I am still learning.
+
+Compile it and run specific puzzle by passing argument which includes day and the task number. Arg format: day-XX-Y 
+
+For example for day one puzzle and task two the right arg is: day1-2
+
+### Run examples 
+```
+binary:
+> ./compiled-binary day1-1
+
+cargo: 
+> cargo run day1-1
+```
---
+++ src/day01.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn solution() -> io::Result<()> {
+    let mut dial: i32 = 50;
+    let max: i32 = 99;
+    let mut zeroes: i32 = 0;
+
+    let path = "inputs/day01-puzzle.txt";
+
+    let file = File::open(Path::new(path))?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        let direction = &line[0..1];
+        let number: i32 = line[1..].parse().unwrap();
+
+        if direction == "R" {
+            dial = (dial + number) % (max + 1);
+        } else {
+            dial = (dial - number) % (max + 1);
+        }
+
+        if dial == 0 {
+            zeroes += 1;
+        }
+
+    }
+
+    println!("\n Password: {zeroes}");
+
+    Ok(())
+}
